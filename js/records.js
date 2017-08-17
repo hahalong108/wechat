321d44f72cd7c60f386c9d5a4852a7b3f741ca77
hahalong108 <longjj_2010@163.com>
2017-08-17 15:28:35 +0800

    add

---
+++ js/records.js
@@ -0,0 +1,17 @@
+// byljj
+var deviceWidth = document.documentElement.clientWidth;
+// if(deviceWidth > 750) deviceWidth = 750;
+document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
+
+
+
+
+
+
+// if ($(window).width() < 350) {
+//     document.documentElement.style.fontSize=document.documentElement.clientWidth/32+'px'; 
+// }else if ($(window).width() > 500) {
+//     document.documentElement.style.fontSize=document.documentElement.clientWidth/75+'px';
+// }else{
+//     document.documentElement.style.fontSize=document.documentElement.clientWidth/37.5+'px'; 
+// }
